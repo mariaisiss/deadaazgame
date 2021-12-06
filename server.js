30f01d87d2a5eab969e3b22cbf1b9f4b9a50da14
LuizHenriqueLobo1 <luizhenriquelobo14@gmail.com>
2021-12-06 00:37:34 -0300

    Configurando ordem das cartas para os clients

--- server.js
+++ server.js
@@ -1,39 +1,37 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'public'))
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'html')
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
-let images = []
-let numberOfPlayers = 1
-
-io.on('connection', (socket) => {
-    let id = socket.id
-    console.log("Novo cliente conectado. ID => " + id)
-    
-    socket.on('setPlayerNumber', playerNumber => {
-        numberOfPlayers += playerNumber;
-    })
-
-    socket.emit('getNumberOfPlayers', numberOfPlayers)
-
-    socket.on('sendImages', currentImages => {
-      images = currentImages  
-    })
-
-    socket.on('toggleCards', id => {
-        socket.broadcast.emit('fodase', id)
-    })
-})
+var numberOfPlayers = 0;
+var currentImages   = [];
 
 app.use('/', (req, res) => {
-    res.render('index.html')
-})
+    res.render('index.html');
+});
 
-server.listen(3000)
+io.on('connection', (socket) => {
+    console.log("Novo cliente conectado. ID => " + socket.id);
+    
+    socket.on('connected', (clientStatus) => {
+        if(clientStatus) {
+            numberOfPlayers++;
+        }
+    });
+
+    socket.emit('getNumberOfPlayers', numberOfPlayers);
+    socket.emit('loadImages', currentImages);
+
+    socket.on('sendImages', (images) => {
+        currentImages = images;
+    });
+});
+
+server.listen(3000);
